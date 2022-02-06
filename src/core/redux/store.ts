fef35a2d6e93fe3aca95f555502507985d461840
SeoKangHyeok <kanghyeok93@naver.com>
2022-02-06 16:15:53 +0900

    feat: #5 Add setting file structure

--- src/core/redux/store.ts
+++ src/core/redux/store.ts
@@ -1,4 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
+import rootReducer from "./rootReducer";
 
-// export type AppStore = ReturnType<typeof makeStore>;
+const makeStore = () =>
+  configureStore({
+    reducer: rootReducer,
+    devTools: true,
+  });
+
+export type AppStore = ReturnType<typeof makeStore>;
+
+export const wrapper = createWrapper<AppStore>(makeStore);
